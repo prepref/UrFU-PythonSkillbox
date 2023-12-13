4901bbe77beadc322bcd33309b290018d3b0457d
prepref <124347607+prepref@users.noreply.github.com>
2023-12-13 22:14:17 +0500

    Update 8.3.py

--- buns/mod8/8.3.py
+++ buns/mod8/8.3.py
@@ -4,6 +4,7 @@ vacancies = pd.read_csv('vacancies_small.csv')
 
 df = pd.DataFrame(vacancies)
 df['salary'] = df[["salary_from", "salary_to"]].sum(axis=1)
+df = df[df.salary_currency == "RUR"]
 df.loc[(((df['salary'] != df['salary_to']) & (df['salary'] == df['salary_from'])) | ((df['salary'] != df['salary_from']) & (df['salary'] == df['salary_to'])))==0,'salary'] = (df['salary_to']+df['salary_from'])/2
 gr = df.groupby(['area_name']).agg({'salary':'mean'}).sort_values(by=['salary','area_name'],ascending=(False,True))
 
